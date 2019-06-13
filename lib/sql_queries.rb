249c09127bd1af903507a32a5d58f7264af04420
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:36:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,7 +4,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, 
   SUM(pledges.amount) 
   FROM projects
-  INNER JOIN pledges on projects.id = pledges.project_id
+  INNER JOIN pledges ON projects.id = pledges.project_id
   GROUP BY projects.title;"
 end
 
