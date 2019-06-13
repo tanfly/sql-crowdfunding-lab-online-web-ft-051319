65ef4a61509a6b1bf599465bc64b3776f7dd4ac7
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:35:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -5,7 +5,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   INNER JOIN pledges
   ON users.id = pledges.user_id
   JOIN projects ON pledges.project_id = projects.id
-  GROUP BY projects.title ASC, pledges.amount;"
+  ORDER BY projects.title ASC, pledges.amount;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
