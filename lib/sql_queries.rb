7eb126d93f620a362ba804ab702d0fa66373ecea
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:47:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,7 +18,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 "SELECT projects.title, 
-  SUM(pledges.amount) - 
+  SUM(pledges.amount) - projects.funding_goal
   "
 end
 
