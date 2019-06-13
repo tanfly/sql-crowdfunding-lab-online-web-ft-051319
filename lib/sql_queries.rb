5d4ae049d186a877c149ca6719e4b9d0e939c222
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:57:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category,
+"SELECT projects.category, pledges.pledges
   SUM (pledges.amount)
   FROM projects 
   INNER JOIN pledges ON pledges.project_id = projects.id
