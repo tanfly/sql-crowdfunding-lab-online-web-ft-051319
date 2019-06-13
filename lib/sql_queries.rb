34755001b4a7dc8307835c906940d1ee3485045a
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:45:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,8 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age
-  SUM(pledges.amount) FROM users
+  SUM(pledges.amount) 
+  FROM users
   INNER JOIN pledges ON pledges.user_id = users.id
   GROUP BY users.name;"
 end
