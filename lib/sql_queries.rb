1722f1281651b81b68c28bf76e4f4704c24326cd
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:40:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   SUM(pledges.amount)
   FROM users
   INNER JOIN pledges ON pledges.user_id = user.id
-  GROUP BY users.name"
+  GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
