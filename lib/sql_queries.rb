80cee60f62af7a17ad996c1127204e49050abfc0
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:57:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,8 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, pledges.pledges,
-  SUM (pledges.amount)
+"SELECT projects.category, pledges.pledges
   FROM projects 
   INNER JOIN pledges ON pledges.project_id = projects.id
   GROUP BY category HAVING category = 'music';"
