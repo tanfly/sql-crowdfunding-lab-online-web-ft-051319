cd2fd87bc38344b19ae3d47714681a48ab0c5513
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:48:35 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,6 +21,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   SUM(pledges.amount) - projects.funding_goal 
   FROM projects 
   INNER JOIN pledges ON pledges.project_id = projects.id
+  
   "
 end
 
