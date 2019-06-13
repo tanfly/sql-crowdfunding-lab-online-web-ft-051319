4759e124641ef884df9bb21bd282d93cc1f6217f
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:53:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   SUM(pledges.amount)
   FROM users 
   INNER JOIN pledges ON pledges.user_id = users.id 
-  GROUP BY user.name 
+  GROUP BY name 
   ORDER BY "
 end
 
