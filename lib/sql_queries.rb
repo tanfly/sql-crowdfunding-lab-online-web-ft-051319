e5de1d651595631384e4065743f9e3b0ae9ea0e9
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:50:15 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,7 +17,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, 
+"SELECT projects.title 
   FROM projects 
   INNER JOIN pledges ON pledges.project_id = projects.id
   GROUP BY title HAVING SUM (pledges.amount) >= projects.funding_goal;
