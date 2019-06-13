19755e2aeac2a19e8e1b1f20774bec681984ae54
Stella McEwen <mcewen.stella@gmail.com>
2019-06-13 23:55:39 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,8 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category
-  SUM (pledges.amount)"
+  SUM (pledges.amount)
+  FROM projects"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
