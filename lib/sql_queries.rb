2a4e7e3a8091b6e94f366f463eb920c19ece096f
Stella McEwen <mcewen.stella@gmail.com>
2019-06-14 00:05:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,12 +36,6 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   
-  "SELECT projects.category, pledges.amount 
-  FROM pledges 
-  LEFT JOIN projects 
-  ON pledges.project_id = projects.id 
-  WHERE projects.category = 'music';" 
-end
 "SELECT projects.category, pledges.amount
   FROM pledges 
   INNER JOIN projects ON pledges.project_id = projects.id
